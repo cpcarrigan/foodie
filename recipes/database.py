ba0ee218e635db726b23de5ef6c46a096bf642a4
Cass Carrigan <cpcarrigan@gmail.com>
2023-10-27 17:54:21 -0700

    adding more database fields

--- recipes/database.py
+++ recipes/database.py
@@ -4,7 +4,25 @@
 """This module provides a database connection."""
 
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlDatabase, QSqlQuerq
+
+
+def _createRecipesTable():
+    """Create the recipes table in the database."""
+    createTableQuery = QSqlQuery()
+    return createTableQuery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS recipes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            author VARCHAR(50),
+            quantity VARCHAR(50),
+            source VARCHAR(50),
+            instructions VARCHAR(50)
+        )
+        """
+    )
+
 
 def createConnection(databaseName):
     """Create and open a database connection."""
@@ -14,9 +32,10 @@ def createConnection(databaseName):
     if not connection.open():
         QMessageBox.warning(
             None,
-            "RP Contact",
+            "Foodie Recipes",
             f"Database Error: {connection.lastError().text()}",
         )
         return False
 
+    _createRecipesTable()
     return True
